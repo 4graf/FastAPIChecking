0d7b9937ca7966ac9eebe858a382a8014ccffa73
4graf <senya.nabatov@mail.ru>
2023-08-22 22:10:11 +0300

    Add app FastAPI and running local server via Uvicorn

--- app/main.py
+++ app/main.py
@@ -1,4 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
 from app.db.database import Base, engine
+from app.routers import client as client_router
+
+
+Base.metadata.create_all(bind=engine)
+app = FastAPI()
+app.include_router(client_router.router, prefix='/user')
 
 if __name__ == "__main__":
-    Base.metadata.create_all(bind=engine)
+    uvicorn.run('main:app', host='localhost', port=8080, reload=True, workers=3)
