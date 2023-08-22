8921b35248946ae6daa47bc30b47dcf4d4620d09
4graf <senya.nabatov@mail.ru>
2023-08-22 03:58:17 +0300

    Create client data schema for queries

---
+++ app/db/schemas/client.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+
+from typing import Optional
+
+from datetime import datetime
+
+
+class ClientBase(BaseModel):
+    name: Optional[str] = None
+    email: Optional[str] = None
+    phone: Optional[str] = None
+    date_registration: Optional[datetime] = None
+
+
+class ClientCreate(ClientBase):
+    name: str
+    email: str
+
+
+class ClientUpdate(ClientBase):
+    pass
+
+
+class ClientDelete(ClientBase):
+    pass
+
+
+class ClientInDBBase(ClientBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Client(ClientInDBBase):
+    pass
+
+
+class ClientInDB(ClientInDBBase):
+    pass
